b4c8bf787c89b28fb9633e3174c6aca0c251bb96
Bobur <ubobur2197@gmail.com>
2026-04-10 15:35:58 +0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,19 @@
+# 1
+roy = [2, 4, 6, 8]
+print([x + 1 for x in roy])
+
+# 2
+roy = ["a", "b", "c"]
+print([x * 3 for x in roy])
+
+# 3
+roy = [7, 14, 21]
+print([x / 7 for x in roy])
+
+# 4
+roy = ["Python", "Java", "C++"]
+print([x.lower() for x in roy])
+
+# 5
+roy = [1, 2, 3, 4]
+print([f"son: {x}" for x in roy])
